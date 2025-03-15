7f0b2cb2f413e8cd08efa0dcd03785c61b59b4f7
Baricus <milesshamo@gmail.com>
2025-03-15 16:42:37 -0500

    Fixed keymap to work with modern qmk
    the qk prepends are gone!

--- keyboards/drop/thekey/keymaps/baricus/keymap.c
+++ keyboards/drop/thekey/keymaps/baricus/keymap.c
@@ -16,66 +16,52 @@
 
 #include QMK_KEYBOARD_H
 
-enum layers {
-	FN_KEYS = 0,
-	COPY_PASTE
-};
+enum layers { FN_KEYS = 0, COPY_PASTE };
 
-enum tap_dancing {
-	TAP_17_20 = 0,
-	TAP_21_24
-};
+enum tap_dancing { TAP_17_20 = 0, TAP_21_24 };
 
 // tap the function keys as needed
 
-
 void handle_taps(int base, int count) {
-	// ensures we have the right max and min values
-	// NOTE: this is TAPS, not offset, so we subtract 1
-	count = count < 1 ? 1 : count;
-	count = count > 4 ? 4 : count;
-	count -= 1;
+    // ensures we have the right max and min values
+    // NOTE: this is TAPS, not offset, so we subtract 1
+    count = count < 1 ? 1 : count;
+    count = count > 4 ? 4 : count;
+    count -= 1;
 
-	if (base == 17) {
-		tap_code(KC_F17 + count);
-	}
-	else if (base == 21) {
-		tap_code(KC_F21 + count);
-	}
-	else {
-		SEND_STRING("YOU FUCKED UP, BUCKO");
-	}
+    if (base == 17) {
+        tap_code(KC_F17 + count);
+    } else if (base == 21) {
+        tap_code(KC_F21 + count);
+    } else {
+        SEND_STRING("YOU FUCKED UP, BUCKO");
+    }
 }
 
 // man I wish I had lambdas :(
-void tap_17(qk_tap_dance_state_t *state, void *user_data) {
-	if (state != NULL) {
-		handle_taps(17, state->count);
-	}
+void tap_17(tap_dance_state_t *state, void *user_data) {
+    if (state != NULL) {
+        handle_taps(17, state->count);
+    }
 }
 
-void tap_21(qk_tap_dance_state_t *state, void *user_data) {
-	if (state != NULL) {
-		handle_taps(21, state->count);
-	}
+void tap_21(tap_dance_state_t *state, void *user_data) {
+    if (state != NULL) {
+        handle_taps(21, state->count);
+    }
 }
 
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
     [TAP_17_20] = ACTION_TAP_DANCE_FN(tap_17),
     [TAP_21_24] = ACTION_TAP_DANCE_FN(tap_21),
 };
 
-
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-	[FN_KEYS] =
-		LAYOUT(
-				/*MO(COPY_PASTE) , TD(TAP_17_20) , TD(TAP_21_24)*/
-				S(KC_GRV), KC_C, KC_V
-			  ),
-	[COPY_PASTE] =
-		LAYOUT(
-				_______        , LCTL(KC_C) , LCTL(KC_V)
-			  ),
+    [FN_KEYS] = LAYOUT(
+        /*MO(COPY_PASTE) , TD(TAP_17_20) , TD(TAP_21_24)*/
+        S(KC_GRV), KC_C, KC_V
+    ),
+    [COPY_PASTE] = LAYOUT(_______, LCTL(KC_C), LCTL(KC_V)),
 
 };
